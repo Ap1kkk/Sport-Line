688dd7d603f4e997e548e6fb2328a7939cc99b2b
Егор Боков <egor.bokov@sitenn.pro>
2024-11-20 19:36:34 +0300

    Initial commit

---
+++ backend/business/src/main/java/ru/gorkycode/ngtu/sportline/business/SportLineApplication.java
@@ -0,0 +1,13 @@
+package ru.gorkycode.ngtu.sportline.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportLineApplication.class, args);
+    }
+
+}
