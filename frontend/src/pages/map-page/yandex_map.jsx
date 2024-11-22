e9fd60be13c9238bc636f41d4d49caf87be7b6a7
Evgeniy <evgeniinaumov04@gmail.com>
2024-11-22 23:46:15 +0300

    added geoposition of user

--- frontend/src/pages/map-page/yandex_map.jsx
+++ frontend/src/pages/map-page/yandex_map.jsx
@@ -1,20 +1,54 @@
-import {YMaps, Map, GeolocationControl} from '@pbe/react-yandex-maps';
-import React from "react";
+import { YMaps, Map, GeolocationControl, Placemark } from '@pbe/react-yandex-maps';
+import React, { useState } from "react";
 
 const YandexMap = () => {
+
+    const [mapInstance, setMapInstance] = useState(null);
+    const [coords, setCoords] = useState(null)
+
+    const handleGeolocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    setCoords([latitude, longitude])
+                    mapInstance.setCenter([latitude, longitude], 12);
+                },
+                (error) => {
+                    console.error('Error getting geolocation: ', error);
+                    alert("Не удалось получить вашу геопозицию.");
+                }
+            );
+        } else {
+            alert("Геолокация не поддерживается вашим браузером.");
+        }
+    };
+
     return (
         <YMaps query={{ apiKey: "71b4ede5-7042-4bba-9243-a2cb4b638bd5" }}>
             <div style={styles.container}>
-                My awesome application with maps!
                 <Map
+                    instanceRef={(ref) => setMapInstance(ref)}
                     defaultState={{
                         center: [56.315309, 43.993506],
                         zoom: 12,
                         controls: [],
-                }}
-                width={"1200px"}
-                height={"700px"}>
-                    <GeolocationControl options={{float: "left"}}/>
+                    }}
+                    width={"800px"}
+                    height={"700px"}>
+
+                    <Placemark
+                        geometry={coords}
+                        options={{
+                            iconLayout: 'default#image',
+                            iconImageHref: 'https://yastatic.net/s3/front-maps-static/maps-front-maps/static/v51/icons/core/map-placemark-dot-32.svg',
+                            iconImageSize: [30, 30],
+                            iconImageOffset: [-10, -10],
+                        }}
+                    />
+
+                    <GeolocationControl onClick={handleGeolocation}>
+                    </GeolocationControl>
                 </Map>
             </div>
         </YMaps>
@@ -24,12 +58,17 @@ const YandexMap = () => {
 const styles = {
     container: {
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '80vh',
-        width: '80%',
+        width: '800px',
+    },
+    button: {
+        margin: '20px',
+        padding: '10px 20px',
+        fontSize: '16px',
+        cursor: 'pointer',
     }
-
 }
 
 export default YandexMap;
