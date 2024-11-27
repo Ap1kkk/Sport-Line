8563b2aaae4094a171d953a826128d91e173889a
Егор Боков <egor.bokov@sitenn.pro>
2024-11-27 23:11:36 +0300

    Fixed unsecured endpoints

--- backend/business/src/main/java/ru/gorkycode/ngtu/sportline/business/system/config/SecurityConfig.java
+++ backend/business/src/main/java/ru/gorkycode/ngtu/sportline/business/system/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((request) ->
                         request
-                                .requestMatchers("/auth/login", "/auth/register", "/auth/validate")
+                                .requestMatchers("/api/v1/auth/login", "/api/v1/auth/register", "/api/v1/auth/validate")
                                 .permitAll()
 
                                 .anyRequest()
