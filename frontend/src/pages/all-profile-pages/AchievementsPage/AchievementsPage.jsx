617370ee89050d78d23dffac81ab374e94a2492b
evgeniy020304 <evgeniinaumov04@gmail.com>
2024-11-30 14:55:01 +0300

    Achievments

--- frontend/src/pages/all-profile-pages/AchievementsPage/AchievementsPage.jsx
+++ frontend/src/pages/all-profile-pages/AchievementsPage/AchievementsPage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import "./AchievementsPage.css";
 import {BASE_API_URL} from "../../../constants/globals";
 
